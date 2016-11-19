fee4be10a7dc77e26c5ae8f0ce422186da29ba01
Declan Whelan <declan@leanintuit.com>
2016-11-18 21:12:12 -0500

    Use gulp for continuous testing.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,15 @@ var mocha = require('gulp-mocha');
 require('./test/support/node.js');
 
 gulp.task('default', function() {
-  return gulp.src(['test/*.js'], { read: false })
+  return gulp.src(['public/**/*.spec.js'], { read: false })
     .pipe(mocha());
+});
+
+gulp.task('test', function() {
+  return gulp.src(['public/**/*.spec.js'], { read: false })
+    .pipe(mocha());
+});
+
+gulp.task('watch-test', function () {
+  gulp.watch(['public/**', 'app.js', 'test/**'], ['test']);
 });
