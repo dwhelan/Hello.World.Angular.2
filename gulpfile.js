ff60f17a0b50e70efaf25849b6cd6fabfff554d2
Declan Whelan <declan@leanintuit.com>
2016-11-18 21:17:58 -0500

    Added better task names for gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var util = require('gulp-util');
 
 require('./test/support/node.js');
 
-gulp.task('default', function() {
-  return gulp.src(['public/**/*.spec.js'], { read: false })
-    .pipe(mocha());
-});
+gulp.task('default', ['test']);
 
 gulp.task('test', function() {
   return gulp.src(['public/**/*.spec.js'], { read: false })
-    .pipe(mocha());
+    .pipe(mocha({ reporter: 'spec' }))
+    .on('error', util.log);
 });
 
 gulp.task('watch-test', function () {
